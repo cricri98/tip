6e971b60c751537c4ac6965239073ad46f663418
chris <chrisjimenez.fba@gmail.com>
2020-08-30 23:39:22 -0700

    first commit

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        print("Hello")
+        view.endEditing(true)
+        
+    }
+    
+    @IBAction func CalculateTip(_ sender: Any) {
+        
+        let bill  = Double(billField.text!) ?? 0
+        
+        let tipPercents = [0.15, 0.18, 0.2]
+        
+        let tip = bill * tipPercents[tipControl.selectedSegmentIndex]
+        
+        let total = tip + bill
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
